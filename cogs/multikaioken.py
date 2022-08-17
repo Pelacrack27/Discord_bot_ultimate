506d946cf974cb68d84edd3f8bed4688fd94fc99
Pelacrack27 <pelayo.muniz@gmail.com>
2022-08-17 23:53:33 +0200

    make it better

--- cogs/multikaioken.py
+++ cogs/multikaioken.py
@@ -89,7 +89,7 @@ class Multikaioken(commands.Cog):
       await ctx.send(
                 "<:SSR_eclair:971672682712141844> Random")
       puntos = puntos + 2
-      for i in range(0, 9):
+    for i in range(0, 9):
             numero = random.randint(1, 10000)
             if numero >= 9500:
                 random2 = random.choice(featured_ssr_kaioken)
@@ -111,18 +111,18 @@ class Multikaioken(commands.Cog):
             else:
                 await ctx.send(
                     "<:R_eclair:971673105024045056> Personaje kk")
-      await ctx.send(f"Total de puntos: {puntos}")
-      if puntos >= 15:
+    await ctx.send(f"Total de puntos: {puntos}")
+    if puntos >= 15:
           await ctx.send("https://i.pinimg.com/564x/4c/4d/88/4c4d8867c58389c11d6d05221aa16632.jpg")
-      elif puntos >= 10:
+    elif puntos >= 10:
           await ctx.send("https://i.pinimg.com/550x/09/19/b6/0919b6dcf57e6a6b4bf31ac5fd1e7928.jpg")
-      elif puntos >= 7:
+    elif puntos >= 7:
           await ctx.send("https://i.ytimg.com/vi/ffHN6_8HDuI/mqdefault.jpg")
-      elif puntos >= 5:
+    elif puntos >= 5:
           await ctx.send("https://i.pinimg.com/736x/35/b7/3e/35b73e01e63b253e041e874aa590681e.jpg")
-      else: 
+    else: 
           await ctx.send("https://pbs.twimg.com/media/EaLXbstWAAETAaT.jpg")
-      await ctx.send("**Multisummon finalizado**")
+    await ctx.send("**Multisummon finalizado**")
 
 
 def setup(client):
